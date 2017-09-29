a1ebed92e441ee2a4c79fd4bae7ba0f1d7742510
Robert van der Mark <robert.vandermark@gmail.com>
2017-09-28 20:37:10 -0400

    react tests passed

--- react/components/Inbox.js
+++ react/components/Inbox.js
@@ -6,12 +6,21 @@ export default class extends React.Component {
 
     constructor() {
         super();
+        this.state = {
+            messages: []
+        }
     }
 
     render() {
+        const { messages } = this.state;
         return (
             <div>
                 <h1>Inbox</h1>
+                {messages.map(message => {
+                    return ( 
+                        <Message fullMessage={message}/>
+                    )
+                })}
             </div>
         );
     }
--- react/components/NewMessageForm.js
+++ react/components/NewMessageForm.js
@@ -1,20 +1,43 @@
 import React from 'react';
 
 export default class extends React.Component {
+    constructor(props){
+        super();
+        this.state = {
+            recipient: '',
+            subject: '',
+            body: ''           
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(evt){
+        const name = evt.target.name;
+        const value = evt.target.value;
+        const obj = {};
+        obj[name] = value;
+        this.setState(obj);
+    }
+
     render() {
+        const { recipient, subject, body } = this.state;
+        const { handleChange } = this;
+        const { onSend } = this.props;
+        const state = this.state;
+
         return (
-            <form>
+            <form onSubmit={() => onSend(state)}>
                 <div className="form-group">
                     <label>To:</label>
-                    <input type="text" id="recipient-field" name="recipient" />
+                    <input onChange={handleChange} value={recipient} type="text" id="recipient-field" name="recipient" />
                 </div>
                 <div className="form-group">
                     <label>Subject:</label>
-                    <input type="text" id="subject-field" name="subject" />
+                    <input value={subject} onChange={handleChange} type="text" id="subject-field" name="subject" />
                 </div>
                 <div className="form-group">
                     <label>Body:</label>
-                    <textarea id="body-field" name="body" />
+                    <textarea onChange={handleChange} value={body} id="body-field" name="body" />
                 </div>
                 <button type="submit">Send Message</button>
             </form>
